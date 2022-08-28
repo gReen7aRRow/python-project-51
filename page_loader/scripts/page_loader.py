24543ca33f2d2df329f6aca99be853fc89247e34
gReen7aRRow <f.gusarov@inbox.ru>
2022-08-28 15:20:59 +0300

    step 2

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
-from page_loader.cli import parse_args
-from page_loader.loader import download
+import logging
+import sys
+
+from page_loader.args import get_args_parser
+from page_loader.engine import download
+
+
+logger = logging.getLogger('base_error')
 
 
 def main():
-    url, path, debug = parse_args()
-    full_path = download(url, path)
-    print(full_path)
+    """Select format selection."""
+    args = get_args_parser().parse_args()
+    try:
+        print(download(args.url, args.output))
+    except Exception:
+        logger.exception("Exception occurred")
+        sys.exit(1)
 
 
 if __name__ == '__main__':
