8350193d0470446e96df10389ce026617e62472a
gReen7aRRow <f.gusarov@inbox.ru>
2022-08-29 11:49:50 +0300

    restyle cli

--- page_loader/cli.py
+++ page_loader/cli.py
@@ -2,12 +2,17 @@ import argparse
 import os
 
 
-def get_args():
+def parse_args():
     parser = argparse.ArgumentParser(description="Page loader")
     parser.add_argument(dest="url", help="URL")
     parser.add_argument("-o", "--output",
                         help="destination for download",
                         default=os.getcwd(),
                         type=str)
+    parser.add_argument("-d", "--debug",
+                        help="activate DEBUG mode",
+                        default=False,
+                        action="store_true")
+    args = parser.parse_args()
 
-    return parser
+    return args.url, args.output, args.debug
